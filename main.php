eb6259dadd5e6587912c986bdc1ad19d06a14d16
nets <nets@gmail.com>
2024-10-01 01:32:22 +0800

    main page fixed

--- main.php
+++ main.php
@@ -2,177 +2,179 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>main</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>AW-K9 Petshop & Grooming Station</title>
     <style>
-table{
-animation: transitionIn-Y-bottom 0.5s;
- }
-body{
-background-image: url(bg.jpg);
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-size: cover;
-background-position: center;
-height: 100%;
-}
+        * {
+            box-sizing: border-box;
+        }       
 
-html, body {
-    height: 100%;
-    margin: 0;
-  }
-  
-.full-height {
-    background: rgba(26, 26, 26, 0.548);
-    background-attachment: fixed;
-    max-height: 100vh;
-    height: 100vh;
+        body, html {
+            margin: 0;
+            padding: 0;
+            font-family: 'Helvetica', sans-serif; 
+            height: 100%;
+            background: url('bg.jpg') no-repeat center center fixed; 
+            background-size: cover;
+            color: #333;
+            overflow-y: auto; 
+        }
 
+        .header {
+            background-color: black;
+            padding: 20px 30px; 
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            position: fixed;
+            top: 0;
+            width: 100%;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
+            z-index: 100;
+        }
 
-}
-table{
-    width: 100%;
-    padding-top: 5px;
-    
-}
-.heading-text{
-    color: white;
-    font-family: helvetica, sans-serif;
-    font-size: 45px;
-    font-weight: 700;
-    line-height: 63px;
-    margin-top: 15%;
-    text-align: center;
-    margin-bottom: 0;
-}
+       
+        .header img {
+            height: 70px; 
+            margin-right: 15px;
+        }
 
-.sub-text2{
-    color: rgba(255, 255, 255, 0.822);
-    font-family: helvetica, sans-serif;
-    font-size: 27px;
-    line-height: 45px;
-    font-weight: 400;
-    text-align: center;
-    margin-top: 0;
-}
+        .header h1 {
+            font-size: 2em; 
+            margin: 0;
+            color: #fff; 
+            display: flex;
+            align-items: center;
+        }
 
+        .buttons {
+            display: flex;
+            gap: 15px;
+        }
 
-.register-btn{
-    background-color: rgba(240, 248, 255, 0.589);
-    font-family: helvetica, sans-serif;
-    color: #345cc4;
-}
+        .btn {
+            background-color: #fff; 
+            color: black;
+            border: none;
+            padding: 10px 20px; 
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 0.9em;
+            font-weight: bold; 
+            letter-spacing: 1px; 
+            transition: background-color 0.3s, transform 0.3s;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); 
+        }
 
+        .btn:hover {
+            background-color: #ccc; 
+            transform: translateY(-2px);
+        }
 
-.logo{
-    color: white;
-    font-family: helvetica, sans-serif;
-    font-weight: bolder;
-    font-size: 50px;
-    padding-left: 20px;
-    animation: transitionIn-Y-over 0.5s;
-}
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: calc(100% - 80px - 60px);
+            margin-top: 80px; 
+            padding: 20px;
+            text-align: center;
+            max-height: calc(100vh - 80px - 60px); 
+            overflow: auto; 
+        }
+        .content {
+            background: rgba(255, 255, 255, 0.8);
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+            max-width: 500px;
+            width: 100%;
+        }
 
-.logo-sub{
-    color: rgba(255, 255, 255, 0.733);
-    font-family: helvetica, sans-serif;
-    font-size: 35px;
+        .content h2 {
+            font-size: 1.8em;
+            margin: 15px 0;
+        }
 
-}
+        .content p {
+            margin: 5px 0;
+            font-size: 1em;
+        }
 
+        .additional-content {
+            margin-top: 15px;
+            font-size: 0.9em;
+            color: #666;
+        }
 
-.nav-item{
-    background-color: #04AA6D;
-    border: none;
-    color: #ffffff;
-    padding: 20px;
-    font-family: helvetica, sans-serif;
-    text-align: center;
-    font-size: 25px;
-    font-weight: 500;
-    margin: 4px 2px;
-    cursor: pointer;
-    border-radius: 12px;
-    transition-duration: 0.4s;
-}
+        @media (max-width: 600px) {
+            .header h1 {
+                font-size: 5vw; 
+            }
 
-.nav-item:hover{
-    background-color: #ffffff;
-    color: #04AA6D;
+            .btn {
+                padding: 8px 15px; 
+                font-size: 0.8em;
+            }
+        }
 
-}
-.button {
-  background-color: #04AA6D; 
-  border: none;
-  color: #ffffff;
-  padding: 20px;
-  font-family: helvetica, sans-serif;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 26px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 12px;
-  transition-duration: 0.4s;
-}
-.button:hover {
-    background-color: #ffffff;
-    color: #04AA6D;
-}
+        .footer {
+            background-color: black;
+            color: white;
+            text-align: center;
+            padding: 20px;
+            position: relative;
+            bottom: 0;
+            width: 100%;
+        }
 
-    </style>
-        
+        .socials {
+            display: flex;
+            justify-content: center;
+            gap: 15px;
+        }
+
+        .social-link {
+            color: white;
+            text-decoration: none;
+            font-size: 1em;
+        }
+
+        .social-link:hover {
+            text-decoration: underline;
+        }
+    </style>  
 </head>
 <body>
-    
-    <div class="full-height">
-        <center>
-        <table border="0">
-            <tr>
-                <td width="80%">
-                    <font class="logo">AW-K9 </font>
-                    <font class="logo-sub">| Petshop & Grooming Station</font>
-                </td>
-                <td width="10%">
-                   <a href="login.php"  class="non-style-link"><p class="nav-item">LOGIN</p></a>
-                </td>
-                <td  width="10%">
-                     <a href="register.php" class="non-style-link"><p class="nav-item" style="padding-right: 10px;">REGISTER</p></a>
-                </td>
-            </tr>
-            <tr>
-                <td  colspan="3">
-                    <p class="heading-text">Less hassle, more happiness.</p>
-                </td>
-            </tr>
-            <tr>
-                <td  colspan="3">
-                    <p class="sub-text2">Book now your grooming for dogs and cats <br> Book now for your Dog hotel reservation needs</p>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="3">
-                    <center>
-                    <a href="login.php">
-                        <input type="button" value="Grooming" class="button" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
-                    </a>
-                    
-                    <a href="login.php">
-                        <input type="button" value="Dog Hotel" class="button" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
-                    </a>
-                </center>
-                </td>
-                
-            </tr>
-            <tr>
-                <td colspan="3">
-                   
-                </td>
-            </tr>
-        </table>
+    <div class="header">
+        <h1>
+            <img src="header-bg.png" alt="AW-K9 Logo"> 
+            Petshop & Grooming Station
+        </h1>
+        <div class="buttons">
+            <a href="login.php" class="btn">LOGIN</a>
+            <a href="register.php" class="btn">REGISTER</a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="content">
+            <h2>Less hassle, more happiness.</h2>
+            <p>Book your grooming for dogs and cats today!</p>
+            <p>Reserve your spot for dog hotel services!</p>
+            <p class="additional-content">Nyarks</p>
+        </div>
+    </div>
+    <div class="footer">
+    <div class="socials">
+    <center>
+        <a href="#" class="social-link">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <p>Copyright 2024. All Rights Reserved</p>
+        <p>AW-K9 Petshop & Grooming Station</p>
     </center>
     </div>
+</div>
 </body>
 </html>
